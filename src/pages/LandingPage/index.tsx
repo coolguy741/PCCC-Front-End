1ea0369defdbd1ae925d7956faef907f714a5cb0
coolguy741 <webreplies2022@gmail.com>
2023-02-27 15:38:48 -0700

    Test landing page setup

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -5,31 +5,31 @@ export const LandingPage = () => {
   return (
     <Container>
       <div className="odd">
-        Panel 1
+        <h1>Panel 1</h1>
         <Spline scene="https://prod.spline.design/75VaWhQ6daSyqN2c/scene.splinecode" />
       </div>
       <div>
-        Panel 2
+        <h1>Panel 2</h1>
         <Spline scene="https://prod.spline.design/CDccCKiwER1WcG8q/scene.splinecode" />
       </div>
       <div className="odd">
-        Panel 3
+        <h1>Panel 3</h1>
         <Spline scene="https://prod.spline.design/nvLk40a823tDdqwD/scene.splinecode" />
       </div>
       <div>
-        Panel 4
+        <h1>Panel 4</h1>
         <Spline scene="https://prod.spline.design/CDccCKiwER1WcG8q/scene.splinecode" />
       </div>
       <div className="odd">
-        Panel 5
+        <h1>Panel 5</h1>
         <Spline scene="https://prod.spline.design/nvLk40a823tDdqwD/scene.splinecode" />
       </div>
       <div>
-        Panel 6
+        <h1>Panel 6</h1>
         <Spline scene="https://prod.spline.design/CDccCKiwER1WcG8q/scene.splinecode" />
       </div>
       <div className="odd">
-        Panel 7
+        <h1>Panel 7</h1>
         <Spline scene="https://prod.spline.design/nvLk40a823tDdqwD/scene.splinecode" />
       </div>
     </Container>
@@ -39,13 +39,27 @@ export const LandingPage = () => {
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  position: relative;
 
   div {
+    position: relative;
     height: 100vh;
     display: flex;
     justify-content: center;
     align-items: center;
     font-size: 3rem;
+
+    h1 {
+      position: absolute;
+      z-index: 1;
+      pointer-events: none;
+    }
+
+    canvas {
+      position: absolute;
+      top: 0;
+      z-index: 0;
+    }
 
     &.odd {
       background-color: #f0f0f0;
