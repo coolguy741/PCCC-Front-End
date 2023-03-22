2eb1a54c6ea3cfd131b41551eca8ff0a6ff35809
AlexAlampour <aalampour@groovejones.com>
2023-03-22 18:55:27 -0500

    feat: made a photo component in accounts folder

--- src/pages/AccountsPage/Profiles/User/index.tsx
+++ src/pages/AccountsPage/Profiles/User/index.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { useLocation } from "react-router-dom";
 import { Button } from "../../../../components/Global/Button";
 import { Icon } from "../../../../components/Global/Icon";
+import { Photo } from "../../../../components/Accounts/Photo";
 
 export const AccountsUserProfilePage = () => {
   //should be deleted after api implementation
@@ -23,20 +24,18 @@ export const AccountsUserProfilePage = () => {
         <Button onClick={handleBack} >&lt; Back</Button>
         <div className="info-container">
           <div className="avatar-container">
+            <div className="avatar">
+              <Photo src={userData.image} role={userData.role}/>
+            </div>
             {
             userData.role === "Standard" ? 
-              <>
-                <div className="avatar"/>
                 <div className="user-info">
                   <p className="bold-big-text">{userData.userID}</p>
                   <p className="text">Birth year: {userData.birthYear}</p>
                   <p className="text">Province: {userData.province}</p>
                   <p className="text">Created: {userData.createdDate}</p>
                 </div>
-              </> 
-            : userData.role === "Standard" ?
-              <>
-                <div className="avatar"/>
+            : userData.role === "Professional" ?
                 <div className="user-info">
                   <p className="bold-big-text">{userData.userID}</p>
                   <p className="bold-text">{userData.name}</p>
@@ -47,10 +46,7 @@ export const AccountsUserProfilePage = () => {
                   <p className="text">{userData.email}</p> 
                   <p className="text">Created: {userData.createdDate}</p>
                 </div>
-              </>
             :
-              <>
-                <div className="avatar"/>
                 <div className="user-info">
                   <p className="bold-big-text">{userData.userID}</p>
                   <p className="bold-text">{userData.name}</p>
@@ -59,24 +55,22 @@ export const AccountsUserProfilePage = () => {
                   <p className="text">{userData.email}</p> 
                   <p className="text">Created: {userData.createdDate}</p>
                 </div>
-              </>
             }
             
           </div>
           <div className="badges-groups-container">
             <h3>Badges</h3>
             <div className="badges">
-              {
-                [...Array(userData.badges)].map((_, index) => (
-                  <span className="badge-icon" key={index}><Icon name="group"/></span>
-                ))
+              {[...Array(userData.badges)].map((_, index) => (
+                <span className="badge-icon" key={index}><Icon name="group"/></span>
+              ))
               }
             </div>
             <h3>Groups</h3>
             <ul className="groups-list">
               {userData.groups.map((group, index) => (
                 <li className="group-item" key={index}>
-                  <span className="group-icon"></span>
+                  <span className="group-icon"><Icon name="group"/></span>
                   <p className="bold-text">{group.name}</p>
                   <p>&lpar {group.number} &rpar</p>
                 </li>
@@ -91,7 +85,11 @@ export const AccountsUserProfilePage = () => {
           {
             userData.activities.map((activity, index) => (
               <li className="activity-item">
-                <div className="activity-name"><span className="group-icon"/>{activity.content}</div>
+                <span className="icon-container">
+                  <Icon name={activity.type}/>
+                </span>
+                <p className="bold-text">User</p>
+                <p className="text"></p>
               </li>
             ))
           }
@@ -102,10 +100,29 @@ export const AccountsUserProfilePage = () => {
 
 const PageContainer = styled.div`
   display: flex;
+  flex-direction: column;
   padding-top: 11px;
 
-  .user-data {
+  Button {
+    max-width: 150px;
+  }
+
+  .info-container {
     display: flex;
     padding-top: 5px;
+
+    .avatar-container{
+      display: flex;
+
+      .avatar {
+        width: 94px;
+        height: 94px;
+      }
+    }
+
+    .badges-groups-container{
+
+    }
+    
   }
 `
