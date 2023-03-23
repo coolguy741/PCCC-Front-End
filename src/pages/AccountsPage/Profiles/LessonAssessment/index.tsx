78628be8d1094fe5ad6b936cd5814a37841f9dfe
AlexAlampour <aalampour@groovejones.com>
2023-03-23 11:11:27 -0500

    feat: made the mockData for accounts/profiles/lessonAssessment

--- src/pages/AccountsPage/Profiles/LessonAssessment/index.tsx
+++ src/pages/AccountsPage/Profiles/LessonAssessment/index.tsx
@@ -1,7 +1,13 @@
+import styled from "styled-components";
+
 export const AccountsUserLessonAssessmentPage = () => {
   return (
-    <>
+    <PageContainer>
       <h1>UserLessonAssessment Page</h1>
-    </>
+    </PageContainer>
   );
 };
+
+const PageContainer = styled.div`
+
+`
