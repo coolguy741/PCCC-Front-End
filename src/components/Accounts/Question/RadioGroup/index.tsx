a7096497f5f6c06a40439666d06419f4b066d698
AlexAlampour <aalampour@groovejones.com>
2023-03-23 17:21:15 -0500

    feat: completed accounts/profiles/:user/:lesson-accessment page

---
+++ src/components/Accounts/Question/RadioGroup/index.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+interface RadioGropQuestionProps {
+  optionContent : string [];
+}
+
+export const RadioGropQuestion = ({optionContent}: RadioGropQuestionProps) => {
+  const [selectedValue, setSelectedValue] = useState('');
+  
+  const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedValue(event.target.value);
+  };
+
+  return (
+    <Container>
+      {
+        optionContent.map((value, index) => (
+          <div key={index}>
+            <input
+              type="radio"
+              id={`option-${index}`}
+              name="options"
+              value={`option-${index}`}
+              checked={selectedValue === `option-${index}`}
+              onChange={handleOptionChange}
+            />
+            <label htmlFor={`option-${index}`}>{value}</label>
+          </div>
+        ))
+      }
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  margin: 10px 0px;
+
+  div {
+    display: flex;
+  }
+`;
--- src/pages/AccountsPage/Profiles/LessonAssessment/index.tsx
+++ src/pages/AccountsPage/Profiles/LessonAssessment/index.tsx
@@ -1,13 +1,56 @@
 import styled from "styled-components";
+import { Button } from "../../../../components/Global/Button";
+import mockData from '../../../../lib/mockData/accounts/profileLessonAccessment.json';
+import { useNavigate } from 'react-router-dom';
+import { Question } from "../../../../components/Accounts/Question";
 
 export const AccountsUserLessonAssessmentPage = () => {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1);
+  }
+
+  const handlePrint = () => {
+  }
+
   return (
     <PageContainer>
-      <h1>UserLessonAssessment Page</h1>
+      <div className="button-container">
+        <Button onClick={handleBack}>&#60; Back</Button>
+        <Button onClick={handlePrint}>Print</Button>
+      </div>
+      <p className="topic-text">Topic : {mockData.topic}</p>
+      <h2 className="title-text">{mockData.lesson}</h2>
+      {
+        mockData.questions.map((question ,index) => (
+          <Question content={question} key={index}/>
+        ))
+      }
     </PageContainer>
   );
 };
 
 const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  .button-container {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 20px;
+  }
+
+  .topic-text {
+    margin-top: 20px;
+    margin-bottom: 0px;
+    padding: 0px;
+  }
+  
+  .title-text {
+    padding: 0px;
+    margin: 0px;
+  }
+
 
 `
