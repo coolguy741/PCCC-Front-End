767689a4b136dae5def97e78e8349d645ee6b62f
AlexAlampour <aalampour@groovejones.com>
2023-03-29 13:31:36 -0500

    feat: made the mockData for recipes/create/preview

--- src/pages/RecipeBuilderPage/RecipesCreatePreviewRecipePage/index.tsx
+++ src/pages/RecipeBuilderPage/RecipesCreatePreviewRecipePage/index.tsx
@@ -1,15 +1,45 @@
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { Button } from "../../../components/Global/Button";
+import { SmallButton } from "../../../components/Global/SmallButton";
 
 export const RecipesCreatePreviewRecipePage = () => {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1);
+  }
+
+  const toggleLanguage = () => {
+      
+  }
+
   return (
     <PageContainer>
-      <h2>RecipesCreatePreviewRecipePage</h2>
+      <LeftAlignedContainer>
+        <Button onClick={handleBack}>Back</Button>
+      </LeftAlignedContainer>
+      <LanguageButtonGroup>
+        <SmallButton onClick={toggleLanguage}>English</SmallButton>
+        <SmallButton onClick={toggleLanguage}>French</SmallButton>
+      </LanguageButtonGroup>
     </PageContainer>
   );
 };
 
 const PageContainer = styled.div`
-  .content {
-    padding: 11px 0px;
-  }
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 `;
+
+const LanguageButtonGroup = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+`
+
+const LeftAlignedContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+`
