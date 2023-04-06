6daed692f5c3a5d0b76be98730ffc546a3d2bf22
AlexAlampour <aalampour@groovejones.com>
2023-04-06 18:09:18 -0500

    completed blocking out the activities

--- src/pages/ActivitiesBuilderPage/ActivitiesCreatePage/index.tsx
+++ src/pages/ActivitiesBuilderPage/ActivitiesCreatePage/index.tsx
@@ -2,20 +2,14 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { Button } from "../../../components/Global/Button";
-import { EditableText } from "../../../components/Global/EditableText";
-import { FileUploader } from "../../../components/Global/FileUploader";
 import { ModalContainer } from "../../../components/Global/ModalContainer";
 import { SmallButton } from "../../../components/Global/SmallButton";
-import { NumberSetter } from "../../../components/Recipes/NumberSetter";
 import { SaveChangesModal } from "../../../components/Global/SaveChangesModal";
-import { SelectTags, Tag } from "../../../components/Recipes/SelectTags";
-import { SelectIngredients, Ingredient } from "../../../components/Recipes/SelectIngredients";
-import mockData from "../../../lib/mockData/recipes/recipesCreate.json";
+import { CenterAlignedContainer, RightAlignedContainer } from "../../../components/Global/Container";
+import { LanguageChooser } from "../../../components/Global/LanguageChooser";
 
 export const ActivitiesCreatePage = () => {
-  const [currentLanguage, setCurrentLanguage] = useState('en');
-  const [ingredients, setIngredients] =  useState<Ingredient[]>([]);
-  const [tags, setTags] = useState<Tag[]>([]);
+  const [lang, setLang] = useState('en');
   const [visibleModal, setVisibleModal] = useState<boolean>(false);
   const navigate = useNavigate();
 
@@ -36,66 +30,29 @@ export const ActivitiesCreatePage = () => {
   }
 
   const handleToggleLanguage = () => {
+    if(lang === "en") setLang("fr");
+    if(lang === "fr") setLang("en");
     setVisibleModal(false);
-    if(currentLanguage === 'en')setCurrentLanguage('fr');
-    if(currentLanguage === 'fr')setCurrentLanguage('en');
   }
 
   return (
     <PageContainer>
-      <Button onClick={handleBack}>Back</Button>
-      <div className="title-buttons-container">
-        <h2>Create recipe</h2>
-        <div className="language-toggle">
-          {currentLanguage == "en" ?
-            <>
-              <SmallButton onClick={() => setCurrentLanguage('en')} bgColor="green">English</SmallButton>
-              <SmallButton onClick={() => setCurrentLanguage('fr')}>French</SmallButton>
-            </>
-            :
-            <>
-              <SmallButton onClick={() => setCurrentLanguage('en')}>English</SmallButton>
-              <ClickedButton onClick={() => setCurrentLanguage('fr')} bgColor="green">French</ClickedButton>
-            </>
-          }
-        </div>
-        <SmallButton onClick={handlePreview}>Preview</SmallButton>
-      </div>
-      <div className="content">
-        <div className="left-content">
-          <h3>Recipe Name</h3>
-          <EditableText text={"Chocolate Granola Bites"} />
-          <h3>What is it good for?</h3>
-          <EditableText text={"These delicious bites are jam packed with fiber to keep you f"} />
-          <div className="images-videos-tags-container">
-            <div style={{width:'65%'}}>
-              <h3>Images and Videos</h3>
-              <FileUploader />
-            </div>
-            <div style={{width:'35%'}}>
-              <h3>Tags</h3>
-              <SelectTags tagOptions={mockData.Tags} tags={tags} setTags={setTags}/>
-            </div>
-          </div>
-        </div>
-        <div className="right-content">
-          <div className="servering-size-container">
-            <h3>Servering Size</h3>
-            <NumberSetter defaultValue={2} />
-          </div>
-          <div>
-            <h3>Ingredients</h3>
-            <SelectIngredients ingredients={ingredients} setIngredients={setIngredients} ingredientOptions={mockData.Ingredients} unitOptions={mockData.Units}/>
-          </div>
-          <div>
-            <h3>Directions</h3>
-            <EditableText text={"Combine all the ingredients together in a large bowl mix until well combined.Refrigerate for about 30 minutes Scoop out a tablespoon portion of mixture, roll each portion in the palm of your hands and place each ball into a resealable container for storage. Bites should be refrigerated to maintain shape or frozen for long term storage."} />
-          </div>
-          <RightAlignedContainer>
-            <SmallButton onClick={showModal}>Save</SmallButton>
-          </RightAlignedContainer>
-        </div>
-      </div>
+      <ButtonGroup>
+        <Button onClick={handleBack}>Back</Button>
+        <Button onClick={handlePreview}>Preview</Button>
+      </ButtonGroup>
+      <h2>Create recipe</h2>
+      <CenterAlignedContainer>
+        <LanguageChooser lang={lang} setLang={setLang} />
+      </CenterAlignedContainer>
+      <ContentContainer>
+        <p>
+          Coming Soon.
+        </p>
+        <RightAlignedContainer>
+          <SmallButton onClick={showModal}>Save</SmallButton>
+        </RightAlignedContainer>
+      </ContentContainer>
       {visibleModal && 
       <ModalContainer>
         <SaveChangesModal onSave={handleSave} onToggleLanguage={handleToggleLanguage}/>
@@ -121,38 +78,41 @@ const PageContainer = styled.div`
       gap: 20px;
     }
   }
-
-  .content{
-    display: flex;
-    gap: 20px;
-
-    .left-content{
-      width: 50%;
-
-      .images-videos-tags-container{
-        display: flex; 
-        justify-content: space-between;
-        gap: 20px;
-
-      }
-    }
-
-    .right-content{
-      display:flex;
-      flex-direction: column;
-      gap: 20px;
-      width: 50%;
-
-      .servering-size-container{
-        display: flex;
-        align-items: center;
-        gap: 20px;
-      }
-    }
-  }
 `;
 
-const RightAlignedContainer = styled.div`
+const ButtonGroup = styled.div`
   display: flex;
-  justify-content: flex-end;
+  justify-content: space-between;
 `
+
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 400px;
+  gap: 20px;
+
+  .left-content{
+    width: 50%;
+
+    .images-videos-tags-container{
+      display: flex; 
+      justify-content: space-between;
+      gap: 20px;
+
+    }
+  }
+
+  .right-content{
+    display:flex;
+    flex-direction: column;
+    gap: 20px;
+    width: 50%;
+
+    .servering-size-container{
+      display: flex;
+      align-items: center;
+      gap: 20px;
+    }
+  }
+`
+
--- src/pages/ActivitiesBuilderPage/ActivitiesEditPage/index.tsx
+++ src/pages/ActivitiesBuilderPage/ActivitiesEditPage/index.tsx
@@ -2,20 +2,14 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { Button } from "../../../components/Global/Button";
-import { EditableText } from "../../../components/Global/EditableText";
-import { FileUploader } from "../../../components/Global/FileUploader";
 import { ModalContainer } from "../../../components/Global/ModalContainer";
 import { SmallButton } from "../../../components/Global/SmallButton";
-import { NumberSetter } from "../../../components/Recipes/NumberSetter";
 import { SaveChangesModal } from "../../../components/Global/SaveChangesModal";
-import { SelectTags, Tag } from "../../../components/Recipes/SelectTags";
-import { SelectIngredients, Ingredient } from "../../../components/Recipes/SelectIngredients";
-import mockData from "../../../lib/mockData/recipes/recipeEdit.json";
+import { CenterAlignedContainer, RightAlignedContainer } from "../../../components/Global/Container";
+import { LanguageChooser } from "../../../components/Global/LanguageChooser";
 
 export const ActivitiesEditPage = () => {
-  const [currentLanguage, setCurrentLanguage] = useState('en');
-  const [ingredients, setIngredients] =  useState<Ingredient[]>(mockData.initialIngredients);
-  const [tags, setTags] = useState<Tag[]>(mockData.initialTags);
+  const [lang, setLang] = useState('en');
   const [visibleModal, setVisibleModal] = useState<boolean>(false);
   const navigate = useNavigate();
 
@@ -24,7 +18,7 @@ export const ActivitiesEditPage = () => {
   }
 
   const handlePreview = () => {
-    navigate("./../../preview");
+    navigate("./../preview");
   }
 
   const showModal = () => {
@@ -32,70 +26,33 @@ export const ActivitiesEditPage = () => {
   }
 
   const handleSave = () => {
-    navigate('./../../preview');
+    navigate('./../preview');
   }
 
   const handleToggleLanguage = () => {
+    if(lang === "en") setLang("fr");
+    if(lang === "fr") setLang("en");
     setVisibleModal(false);
-    if(currentLanguage === 'en')setCurrentLanguage('fr');
-    if(currentLanguage === 'fr')setCurrentLanguage('en');
   }
 
   return (
     <PageContainer>
-      <Button onClick={handleBack}>Back</Button>
-      <div className="title-buttons-container">
-        <h2>Edit recipe</h2>
-        <div className="language-toggle">
-          {currentLanguage == "en" ?
-            <>
-              <SmallButton onClick={() => setCurrentLanguage('en')} bgColor="green">English</SmallButton>
-              <SmallButton onClick={() => setCurrentLanguage('fr')}>French</SmallButton>
-            </>
-            :
-            <>
-              <SmallButton onClick={() => setCurrentLanguage('en')}>English</SmallButton>
-              <SmallButton onClick={() => setCurrentLanguage('fr')} bgColor="green">French</SmallButton>
-            </>
-          }
-        </div>
-        <SmallButton onClick={handlePreview}>Preview</SmallButton>
-      </div>
-      <div className="content">
-        <div className="left-content">
-          <h3>Recipe Name</h3>
-          <EditableText text={mockData.recipeName} />
-          <h3>What is it good for?</h3>
-          <EditableText text={mockData.advantage} />
-          <div className="images-videos-tags-container">
-            <div style={{width:'65%'}}>
-              <h3>Images and Videos</h3>
-              <FileUploader />
-            </div>
-            <div style={{width:'35%'}}>
-              <h3>Tags</h3>
-              <SelectTags tagOptions={mockData.Tags} tags={tags} setTags={setTags}/>
-            </div>
-          </div>
-        </div>
-        <div className="right-content">
-          <div className="servering-size-container">
-            <h3>Servering Size</h3>
-            <NumberSetter defaultValue={2} />
-          </div>
-          <div>
-            <h3>Ingredients</h3>
-            <SelectIngredients ingredients={ingredients} setIngredients={setIngredients} ingredientOptions={mockData.Ingredients} unitOptions={mockData.Units}/>
-          </div>
-          <div>
-            <h3>Directions</h3>
-            <EditableText text={mockData.directions} />
-          </div>
-          <RightAlignedContainer>
-            <SmallButton onClick={showModal}>Save changes</SmallButton>
-          </RightAlignedContainer>
-        </div>
-      </div>
+      <ButtonGroup>
+        <Button onClick={handleBack}>Back</Button>
+        <Button onClick={handlePreview}>Preview</Button>
+      </ButtonGroup>
+      <h2>Create recipe</h2>
+      <CenterAlignedContainer>
+        <LanguageChooser lang={lang} setLang={setLang} />
+      </CenterAlignedContainer>
+      <ContentContainer>
+        <p>
+          Coming Soon.
+        </p>
+        <RightAlignedContainer>
+          <SmallButton onClick={showModal}>Save</SmallButton>
+        </RightAlignedContainer>
+      </ContentContainer>
       {visibleModal && 
       <ModalContainer>
         <SaveChangesModal onSave={handleSave} onToggleLanguage={handleToggleLanguage}/>
@@ -121,38 +78,41 @@ const PageContainer = styled.div`
       gap: 20px;
     }
   }
-
-  .content{
-    display: flex;
-    gap: 20px;
-
-    .left-content{
-      width: 50%;
-
-      .images-videos-tags-container{
-        display: flex; 
-        justify-content: space-between;
-        gap: 20px;
-
-      }
-    }
-
-    .right-content{
-      display:flex;
-      flex-direction: column;
-      gap: 20px;
-      width: 50%;
-
-      .servering-size-container{
-        display: flex;
-        align-items: center;
-        gap: 20px;
-      }
-    }
-  }
 `;
 
-const RightAlignedContainer = styled.div`
+const ButtonGroup = styled.div`
   display: flex;
-  justify-content: flex-end;
+  justify-content: space-between;
 `
+
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 400px;
+  gap: 20px;
+
+  .left-content{
+    width: 50%;
+
+    .images-videos-tags-container{
+      display: flex; 
+      justify-content: space-between;
+      gap: 20px;
+
+    }
+  }
+
+  .right-content{
+    display:flex;
+    flex-direction: column;
+    gap: 20px;
+    width: 50%;
+
+    .servering-size-container{
+      display: flex;
+      align-items: center;
+      gap: 20px;
+    }
+  }
+`
+
