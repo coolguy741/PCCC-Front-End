a9a1ec15e8b9a0ae92406bdd0128c676a6f72ea8
nmomoh <nobert@groovejones.com>
2023-04-11 16:06:42 +0100

    added eslint & prettier config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import basicSsl from "@vitejs/plugin-basic-ssl";
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { https: true },
-  plugins: [react(), basicSsl()],
+  plugins: [react(), basicSsl(), eslint()],
 });
