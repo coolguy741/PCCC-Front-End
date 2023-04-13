cee0381e136daa8144fa435771d46ed4664e91d5
nmomoh <nobert@groovejones.com>
2023-04-13 02:49:35 +0100

    added assets

---
+++ src/components/Icons/index.ts
@@ -0,0 +1,5 @@
+import DirectionLeft from "./DirectionLeft/directionLeft";
+import PCCRed from "./PCCRed/pccRed";
+import PFKLogo from "./PFKLogo/PFKLogo";
+
+export { PFKLogo, DirectionLeft, PCCRed };
