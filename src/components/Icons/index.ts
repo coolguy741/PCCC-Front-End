ec14a81eedc5a296c626e03009a8a4db842e88c7
nmomoh <nobert@groovejones.com>
2023-04-18 01:22:54 +0100

    added background elements

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,7 +1,21 @@
+import AppleBG from "./AppleBG/appleBG";
 import ArrowRight from "./ArrowRight/arrowRight";
 import DirectionLeft from "./DirectionLeft/directionLeft";
+import GrapeBG from "./GrapeBG/grapeBG";
 import Info from "./Info/info";
+import LemonBG from "./LemonBG/lemonBG";
+import OrangeBG from "./OrangeBG/orangeBG";
 import PCCRed from "./PCCRed/pccRed";
 import PFKLogo from "./PFKLogo/PFKLogo";
 
-export { PFKLogo, DirectionLeft, PCCRed, ArrowRight, Info };
+export {
+  PFKLogo,
+  LemonBG,
+  DirectionLeft,
+  PCCRed,
+  ArrowRight,
+  Info,
+  GrapeBG,
+  OrangeBG,
+  AppleBG,
+};
