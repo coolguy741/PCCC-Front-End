2306d23609250570a9232b6e1de92a443fc25394
AlexAlampour <aalampour@groovejones.com>
2023-04-21 14:26:54 -0500

    feat: made a basic ContentListPageTemplateAdmin

---
+++ src/components/Global/ContentListPageTemAdmin/index.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const ContentListPageTemAdmin = () => {
+  return <Style.Container>ContentListPageTemAdmin</Style.Container>;
+};
+
+const Style = {
+  Container: styled.div`
+    display: flex;
+    gap: 20px;
+  `,
+};
