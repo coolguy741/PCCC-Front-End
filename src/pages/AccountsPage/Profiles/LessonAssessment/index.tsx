f2d957888d081b3ee2f17ad896f4fe730197ecde
AlexAlampour <aalampour@groovejones.com>
2023-04-28 17:56:32 -0500

    made the lessonAssessment page

--- src/pages/AccountsPage/Profiles/LessonAssessment/index.tsx
+++ src/pages/AccountsPage/Profiles/LessonAssessment/index.tsx
@@ -1,8 +1,14 @@
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { Navigation, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 import Button from "../../../../components/Button";
 import { BackButton } from "../../../../components/Global/BackButton";
 import mockData from "../../../../lib/mockData/accounts/profileLessonAccessment.json";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import { Question } from "../../../../components/Accounts/Question";
 
 export const AccountsUserLessonAssessmentPage = () => {
   const navigate = useNavigate();
@@ -25,7 +31,31 @@ export const AccountsUserLessonAssessmentPage = () => {
       </Style.ButtonContainer>
       <Style.Topic>{"Topic : " + mockData.topic}</Style.Topic>
       <Style.Lesson>{mockData.lesson}</Style.Lesson>
-      <Style.Questions></Style.Questions>
+      <Style.Questions>
+        <Swiper
+          speed={600}
+          slidesPerView={"auto"}
+          spaceBetween={300}
+          centeredSlides={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className="mySwiper"
+        >
+          {mockData.questions.map((question, index) => (
+            <SwiperSlide>
+              <Question
+                content={question}
+                pattern={"apple"}
+                count={mockData.questions.length}
+                order={index + 1}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Style.Questions>
     </Style.PageContainer>
   );
 };
@@ -61,12 +91,34 @@ const Style = {
     margin-left: 8%;
   `,
   Lesson: styled.h2`
-    margin-top: 18px;
+    margin-top: 20px;
+    margin-bottom: 24px;
     margin-left: 8%;
     font-weight: 600;
     font-size: 33px;
     line-height: 40px;
     color: var(--neutral-800);
   `,
-  Questions: styled.div``,
+  Questions: styled.div`
+    width: 100%;
+    height: calc(100vh - 600px);
+    .swiper {
+      width: 100%;
+      height: 100%;
+    }
+    .swiper-slide {
+      width: 84%;
+    }
+    .swiper-button-prev,
+    .swiper-button-next {
+      width: 77px;
+      height: 77px;
+      border-radius: 50%;
+      background-color: white;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      filter: drop-shadow(0px 4px 16px rgba(0, 0, 0, 0.1));
+    }
+  `,
 };
