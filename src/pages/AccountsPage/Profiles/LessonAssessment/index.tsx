fd75e26d40219e0214e067f16318da24856560a8
AlexAlampour <aalampour@groovejones.com>
2023-04-28 08:06:34 -0500

    made the page layout

--- src/pages/AccountsPage/Profiles/LessonAssessment/index.tsx
+++ src/pages/AccountsPage/Profiles/LessonAssessment/index.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { Question } from "../../../../components/Accounts/Question";
-import { Button } from "../../../../components/Global/Button";
+import Button from "../../../../components/Button";
+import { BackButton } from "../../../../components/Global/BackButton";
 import mockData from "../../../../lib/mockData/accounts/profileLessonAccessment.json";
 
 export const AccountsUserLessonAssessmentPage = () => {
@@ -17,15 +17,15 @@ export const AccountsUserLessonAssessmentPage = () => {
 
   return (
     <Style.PageContainer>
-      <div className="button-container">
-        <Button onClick={handleBack}>&#60; Back</Button>
-        <Button onClick={handlePrint}>Print</Button>
-      </div>
-      <p className="topic-text">Topic : {mockData.topic}</p>
-      <h2 className="title-text">{mockData.lesson}</h2>
-      {mockData.questions.map((question, index) => (
-        <Question content={question} key={index} />
-      ))}
+      <Style.ButtonContainer>
+        <BackButton onClick={handleBack} />
+        <Button onClick={handlePrint} size="large">
+          Print
+        </Button>
+      </Style.ButtonContainer>
+      <Style.Topic>{"Topic : " + mockData.topic}</Style.Topic>
+      <Style.Lesson>{mockData.lesson}</Style.Lesson>
+      <Style.Questions></Style.Questions>
     </Style.PageContainer>
   );
 };
@@ -34,22 +34,39 @@ const Style = {
   PageContainer: styled.div`
     display: flex;
     flex-direction: column;
-
-    .button-container {
-      display: flex;
-      justify-content: space-between;
-      margin-top: 20px;
-    }
-
-    .topic-text {
-      margin-top: 20px;
-      margin-bottom: 0px;
-      padding: 0px;
-    }
-
-    .title-text {
-      padding: 0px;
-      margin: 0px;
-    }
+    justify-content: center;
+    align-items: flex-start;
+    font-family: "Noir Std";
+    font-style: normal;
   `,
+  ButtonContainer: styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `,
+  Topic: styled.p`
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 10px 16px;
+    gap: 10px;
+    border: 2px solid var(--orange-600);
+    border-radius: 50px;
+    color: var(--orange-600);
+    text-transform: uppercase;
+    margin-top: 40px;
+    margin-left: 8%;
+  `,
+  Lesson: styled.h2`
+    margin-top: 18px;
+    margin-left: 8%;
+    font-weight: 600;
+    font-size: 33px;
+    line-height: 40px;
+    color: var(--neutral-800);
+  `,
+  Questions: styled.div``,
 };
--- src/pages/AccountsPage/Profiles/LessonAssessmentPrint/index.tsx
+++ src/pages/AccountsPage/Profiles/LessonAssessmentPrint/index.tsx
@@ -1,15 +1,11 @@
 import styled from "styled-components";
-import { Question } from "../../../../components/Accounts/Question";
 import mockData from "../../../../lib/mockData/accounts/profileLessonAccessment.json";
 
 export const AccountsUserLessonAssessmentPrintPage = () => {
   return (
     <Style.PageContainer>
-      <p className="topic-text">Topic : {mockData.topic}</p>
+      <p>Topic : {mockData.topic}</p>
       <h2 className="title-text">{mockData.lesson}</h2>
-      {mockData.questions.map((question, index) => (
-        <Question content={question} key={index} />
-      ))}
     </Style.PageContainer>
   );
 };
