a269a534bc68a7376a51376adfa4bc3fd090280f
coolguy741 <webreplies2022@gmail.com>
2023-05-01 13:24:15 -0600

    Housekeeping, redirect to login after sign up

--- src/lib/api/helpers/getAuthenticatedUser.ts
+++ src/lib/api/helpers/getAuthenticatedUser.ts
@@ -3,30 +3,6 @@ import { setUser } from "../../../stores/userStore";
 
 export const getAuthenticatedUser = async () => {
   const { api } = getAPI();
-  // const cookies = document.cookie;
-  // const value = `; ${cookies}`;
-  // const parts = value.split("; PCCC_TOKEN=");
-
-  // console.log(cookies);
-
-  // if (parts.length === 2) {
-  //   const token = parts?.pop()?.split(";").shift();
-
-  //   if (token) {
-  //     const response = await api.appUserUserProfileList({
-  //       headers: {
-  //         Authorization: `Bearer ${token}`,
-  //       },
-  //     });
-
-  //     if (response.status === 200) {
-  //       console.log("SET USER");
-
-  //       setUser(response.data);
-  //     }
-  //   }
-  // }
-
   const name = "PCCC_TOKEN=";
   const decodedCookie = decodeURIComponent(document.cookie);
   const ca = decodedCookie.split(";");
