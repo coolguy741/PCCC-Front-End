57d8a741dfe699016acd0deeb9c5ee3fa9091b9d
coolguy741 <webreplies2022@gmail.com>
2023-05-01 11:21:38 -0600

    Auth token fix

--- src/lib/api/helpers/getAuthenticatedUser.ts
+++ src/lib/api/helpers/getAuthenticatedUser.ts
@@ -3,25 +3,53 @@ import { setUser } from "../../../stores/userStore";
 
 export const getAuthenticatedUser = async () => {
   const { api } = getAPI();
-  const cookies = document.cookie;
-  const value = `; ${cookies}`;
-  const parts = value.split("; PCCC_TOKEN=");
+  // const cookies = document.cookie;
+  // const value = `; ${cookies}`;
+  // const parts = value.split("; PCCC_TOKEN=");
 
-  if (parts.length === 2) {
-    const token = parts?.pop()?.split(";").shift();
+  // console.log(cookies);
 
-    if (token) {
+  // if (parts.length === 2) {
+  //   const token = parts?.pop()?.split(";").shift();
+
+  //   if (token) {
+  //     const response = await api.appUserUserProfileList({
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //       },
+  //     });
+
+  //     if (response.status === 200) {
+  //       console.log("SET USER");
+
+  //       setUser(response.data);
+  //     }
+  //   }
+  // }
+
+  const name = "PCCC_TOKEN=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(";");
+
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+
+    if (c.indexOf(name) == 0) {
       const response = await api.appUserUserProfileList({
         headers: {
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${c.substring(name.length, c.length)}`,
         },
       });
 
       if (response.status === 200) {
-        console.log("SET USER");
-
         setUser(response.data);
       }
     }
   }
+
+  return "";
 };
