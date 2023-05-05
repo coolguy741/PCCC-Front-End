b2c02d6863979e5ec79a8bcdf30785d49f21edae
AlexAlampour <aalampour@groovejones.com>
2023-05-05 16:55:21 -0500

    completed the grocery list print page

--- src/components/Global/Logo/Print.tsx
+++ src/components/Global/Logo/Print.tsx
@@ -1,7 +1,31 @@
+import styled from "styled-components";
+
 interface LogoProps {
   width?: number;
+  type?: "colored" | "white";
 }
 
-export const PrintLogo = ({ width = 80 }: LogoProps) => {
-  return <img src="/images/logo-print.svg" alt="logo print" width={width} />;
+export const PrintLogo = ({ width = 80, type = "colored" }: LogoProps) => {
+  return (
+    <>
+      {" "}
+      {type === "colored" ? (
+        <Style.Img
+          src="/images/logo-print.svg"
+          alt="logo print"
+          width={width}
+        />
+      ) : (
+        <Style.Img src="/images/logo-en.svg" alt="logo print" width={width} />
+      )}
+    </>
+  );
+};
+
+const Style = {
+  Img: styled.img`
+    @media print {
+      width: 48px !important;
+    }
+  `,
 };
