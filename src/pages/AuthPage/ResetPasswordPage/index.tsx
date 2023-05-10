06f74caa745d131fa333421beb4e2502b2cbd380
Momoh Nobert <okhai0112@gmail.com>
2023-05-10 15:22:57 +0100

    added icon

--- src/pages/AuthPage/ResetPasswordPage/index.tsx
+++ src/pages/AuthPage/ResetPasswordPage/index.tsx
@@ -1,7 +1,9 @@
 import { useNavigate } from "react-router-dom";
 import { ResetPasswordForm } from "../../../components/Auth/ResetPasswordForm";
 import { BackButton } from "../../../components/Global/BackButton";
+import { OrangeBG } from "../../../components/Icons";
 import { AuthLayout } from "../../../layouts/AuthLayout/authLayout";
+import { rightSideAnimationProps } from "../../../styles/animations/auth";
 
 export const ResetPasswordPage = () => {
   const navigate = useNavigate();
@@ -11,6 +13,9 @@ export const ResetPasswordPage = () => {
       <span className="auth-breadcrumb">
         <BackButton />
       </span>
+      <div className="auth-image">
+        <OrangeBG key="educator-recovery" {...rightSideAnimationProps} />
+      </div>
       <ResetPasswordForm />
     </AuthLayout>
   );
