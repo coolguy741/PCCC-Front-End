5e6f0388cab48800808dd4131cb74da3a47e16b6
nmomoh <nobert@groovejones.com>
2023-05-16 19:30:48 +0100

    switched to 16px glass background

--- src/styles/helpers/glassBackground.tsx
+++ src/styles/helpers/glassBackground.tsx
@@ -6,7 +6,7 @@ export function glassBackground() {
     background: rgba(255, 255, 255, 0.5);
     box-shadow: 0px 7.78814px 19.4703px rgba(0, 0, 0, 0.1);
     backdrop-filter: blur(59.2764px);
-    border-radius: 32px;
+    border-radius: 16px;
     padding: ${conv(40, "vh")} ${conv(40, "vw")};
   `;
 }
