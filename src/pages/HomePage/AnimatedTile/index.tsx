6ffaf8fc5f0d7224d987d29b3aa0ccd81aab9f16
nmomoh <nobert@groovejones.com>
2023-05-16 23:48:13 +0100

    added todo

---
+++ src/pages/HomePage/AnimatedTile/index.tsx
@@ -0,0 +1,7 @@
+// TODO: Create animated tile functionality.
+// needed?
+// title (component with color variant span)
+// imageOrSpline (currently image, spline intended)
+// text (details)
+// image width & content width.
+// button action.
