57f41bde86b74022598c06103e1e9036c3d1c5b1
coolguy741 <webreplies2022@gmail.com>
2023-05-17 08:44:51 -0600

    Lint fix

--- src/components/MealPlanner/Plan/MealPlans/mocks.ts
+++ src/components/MealPlanner/Plan/MealPlans/mocks.ts
@@ -1,192 +1,191 @@
 export const mockMealPlans = [
-    {
-      day: "sunday",
-      plans: [
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-0.png",
-        },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-1.png",
-        },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-2.png",
-        },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-3.png",
-        },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-4.png",
-        },
-      ],
-    },
-    {
-      day: "monday",
-      plans: [
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-5.png",
-        },
-        { description: null },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-6.png",
-        },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-7.png",
-        },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-8.png",
-        },
-      ],
-    },
-    {
-      day: "tuesday",
-      plans: [
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-10.png",
-        },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-11.png",
-        },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-12.png",
-        },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-13.png",
-        },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-14.png",
-        },
-      ],
-    },
-    {
-      day: "wednesday",
-      plans: [
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-15.png",
-        },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-16.png",
-        },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-17.png",
-        },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-19.png",
-        },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-20.png",
-        },
-      ],
-    },
-    {
-      day: "thursday",
-      plans: [
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-21.png",
-        },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-22.png",
-        },
-        { description: null },
-        { description: null },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-23.png",
-        },
-      ],
-    },
-    {
-      day: "friday",
-      plans: [
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-24.png",
-        },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-26.png",
-        },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-27.png",
-        },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-28.png",
-        },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-29.png",
-        },
-      ],
-    },
-    {
-      day: "saturday",
-      plans: [
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-30.png",
-        },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-31.png",
-        },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-32.png",
-        },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-33.png",
-        },
-        {
-          description: "Roasted red pepper hummusRoasted red pepper hummus...",
-          image: "/images/plate-full-planner/meals/image-0.png",
-        },
-      ],
-    },
-  ];
-  
-  export const mockMealPlanMenu = [
-    {
-      description: "Roasted red pepper hummusRoasted red pepper hummus...",
-      image: "/images/plate-full-planner/scroll-menu/image-1.svg",
-    },
-    {
-      description: "Roasted red pepper hummusRoasted red pepper hummus...",
-      image: "/images/plate-full-planner/scroll-menu/image-2.svg",
-    },
-    {
-      description: "Roasted red pepper hummusRoasted red pepper hummus...",
-      image: "/images/plate-full-planner/scroll-menu/image-3.svg",
-    },
-    {
-      description: "Roasted red pepper hummusRoasted red pepper hummus...",
-      image: "/images/plate-full-planner/scroll-menu/image-1.svg",
-    },
-    {
-      description: "Roasted red pepper hummusRoasted red pepper hummus...",
-      image: "/images/plate-full-planner/scroll-menu/image-2.svg",
-    },
-  ];
-  
+  {
+    day: "sunday",
+    plans: [
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-0.png",
+      },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-1.png",
+      },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-2.png",
+      },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-3.png",
+      },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-4.png",
+      },
+    ],
+  },
+  {
+    day: "monday",
+    plans: [
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-5.png",
+      },
+      { description: null },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-6.png",
+      },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-7.png",
+      },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-8.png",
+      },
+    ],
+  },
+  {
+    day: "tuesday",
+    plans: [
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-10.png",
+      },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-11.png",
+      },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-12.png",
+      },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-13.png",
+      },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-14.png",
+      },
+    ],
+  },
+  {
+    day: "wednesday",
+    plans: [
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-15.png",
+      },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-16.png",
+      },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-17.png",
+      },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-19.png",
+      },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-20.png",
+      },
+    ],
+  },
+  {
+    day: "thursday",
+    plans: [
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-21.png",
+      },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-22.png",
+      },
+      { description: null },
+      { description: null },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-23.png",
+      },
+    ],
+  },
+  {
+    day: "friday",
+    plans: [
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-24.png",
+      },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-26.png",
+      },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-27.png",
+      },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-28.png",
+      },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-29.png",
+      },
+    ],
+  },
+  {
+    day: "saturday",
+    plans: [
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-30.png",
+      },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-31.png",
+      },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-32.png",
+      },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-33.png",
+      },
+      {
+        description: "Roasted red pepper hummusRoasted red pepper hummus...",
+        image: "/images/plate-full-planner/meals/image-0.png",
+      },
+    ],
+  },
+];
+
+export const mockMealPlanMenu = [
+  {
+    description: "Roasted red pepper hummusRoasted red pepper hummus...",
+    image: "/images/plate-full-planner/scroll-menu/image-1.svg",
+  },
+  {
+    description: "Roasted red pepper hummusRoasted red pepper hummus...",
+    image: "/images/plate-full-planner/scroll-menu/image-2.svg",
+  },
+  {
+    description: "Roasted red pepper hummusRoasted red pepper hummus...",
+    image: "/images/plate-full-planner/scroll-menu/image-3.svg",
+  },
+  {
+    description: "Roasted red pepper hummusRoasted red pepper hummus...",
+    image: "/images/plate-full-planner/scroll-menu/image-1.svg",
+  },
+  {
+    description: "Roasted red pepper hummusRoasted red pepper hummus...",
+    image: "/images/plate-full-planner/scroll-menu/image-2.svg",
+  },
+];
