8213eed7bcea02f145a0c21d32f79e3b21e6fb88
coolguy741 <webreplies2022@gmail.com>
2023-05-23 15:48:00 -0600

    Image case fix for AWS

--- src/components/MealPlanner/Plan/MealPlans/mocks.ts
+++ src/components/MealPlanner/Plan/MealPlans/mocks.ts
@@ -4,23 +4,23 @@ export const mockMealPlans = [
     plans: [
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-0.png",
+        image: "/images/plate-full-planner/meals/image0.png",
       },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-1.png",
+        image: "/images/plate-full-planner/meals/image1.png",
       },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-2.png",
+        image: "/images/plate-full-planner/meals/image2.png",
       },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-3.png",
+        image: "/images/plate-full-planner/meals/image3.png",
       },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-4.png",
+        image: "/images/plate-full-planner/meals/image4.png",
       },
     ],
   },
@@ -29,20 +29,20 @@ export const mockMealPlans = [
     plans: [
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-5.png",
+        image: "/images/plate-full-planner/meals/image5.png",
       },
       { description: null },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-6.png",
+        image: "/images/plate-full-planner/meals/image6.png",
       },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-7.png",
+        image: "/images/plate-full-planner/meals/image7.png",
       },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-8.png",
+        image: "/images/plate-full-planner/meals/image8.png",
       },
     ],
   },
@@ -51,23 +51,23 @@ export const mockMealPlans = [
     plans: [
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-10.png",
+        image: "/images/plate-full-planner/meals/image10.png",
       },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-11.png",
+        image: "/images/plate-full-planner/meals/image11.png",
       },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-12.png",
+        image: "/images/plate-full-planner/meals/image12.png",
       },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-13.png",
+        image: "/images/plate-full-planner/meals/image13.png",
       },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-14.png",
+        image: "/images/plate-full-planner/meals/image14.png",
       },
     ],
   },
@@ -76,23 +76,23 @@ export const mockMealPlans = [
     plans: [
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-15.png",
+        image: "/images/plate-full-planner/meals/image15.png",
       },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-16.png",
+        image: "/images/plate-full-planner/meals/image16.png",
       },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-17.png",
+        image: "/images/plate-full-planner/meals/image17.png",
       },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-19.png",
+        image: "/images/plate-full-planner/meals/image19.png",
       },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-20.png",
+        image: "/images/plate-full-planner/meals/image20.png",
       },
     ],
   },
@@ -101,17 +101,17 @@ export const mockMealPlans = [
     plans: [
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-21.png",
+        image: "/images/plate-full-planner/meals/image21.png",
       },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-22.png",
+        image: "/images/plate-full-planner/meals/image22.png",
       },
       { description: null },
       { description: null },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-23.png",
+        image: "/images/plate-full-planner/meals/image23.png",
       },
     ],
   },
@@ -120,23 +120,23 @@ export const mockMealPlans = [
     plans: [
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-24.png",
+        image: "/images/plate-full-planner/meals/image24.png",
       },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-26.png",
+        image: "/images/plate-full-planner/meals/image26.png",
       },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-27.png",
+        image: "/images/plate-full-planner/meals/image27.png",
       },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-28.png",
+        image: "/images/plate-full-planner/meals/image28.png",
       },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-29.png",
+        image: "/images/plate-full-planner/meals/image29.png",
       },
     ],
   },
@@ -145,23 +145,23 @@ export const mockMealPlans = [
     plans: [
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-30.png",
+        image: "/images/plate-full-planner/meals/image30.png",
       },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-31.png",
+        image: "/images/plate-full-planner/meals/image31.png",
       },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-32.png",
+        image: "/images/plate-full-planner/meals/image32.png",
       },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-33.png",
+        image: "/images/plate-full-planner/meals/image33.png",
       },
       {
         description: "Roasted red pepper hummusRoasted red pepper hummus...",
-        image: "/images/plate-full-planner/meals/image-0.png",
+        image: "/images/plate-full-planner/meals/image0.png",
       },
     ],
   },
@@ -170,22 +170,22 @@ export const mockMealPlans = [
 export const mockMealPlanMenu = [
   {
     description: "Roasted red pepper hummusRoasted red pepper hummus...",
-    image: "/images/plate-full-planner/scroll-menu/image-1.svg",
+    image: "/images/plate-full-planner/scroll-menu/image1.svg",
   },
   {
     description: "Roasted red pepper hummusRoasted red pepper hummus...",
-    image: "/images/plate-full-planner/scroll-menu/image-2.svg",
+    image: "/images/plate-full-planner/scroll-menu/image2.svg",
   },
   {
     description: "Roasted red pepper hummusRoasted red pepper hummus...",
-    image: "/images/plate-full-planner/scroll-menu/image-3.svg",
+    image: "/images/plate-full-planner/scroll-menu/image3.svg",
   },
   {
     description: "Roasted red pepper hummusRoasted red pepper hummus...",
-    image: "/images/plate-full-planner/scroll-menu/image-1.svg",
+    image: "/images/plate-full-planner/scroll-menu/image1.svg",
   },
   {
     description: "Roasted red pepper hummusRoasted red pepper hummus...",
-    image: "/images/plate-full-planner/scroll-menu/image-2.svg",
+    image: "/images/plate-full-planner/scroll-menu/image2.svg",
   },
 ];
