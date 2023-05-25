8e35578a3d90ca74e2b5a39532a58431db45d2e5
AlexAlampour <aalampour@groovejones.com>
2023-05-25 08:54:46 -0500

    replaced scrollbar container and implemented convertToRelativeUnit

--- src/pages/NotificationsPage/index.tsx
+++ src/pages/NotificationsPage/index.tsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 import Button from "../../components/Button";
+import Scrollbar from "../../components/Global/Scrollbar";
 import { avatars_data } from "../../lib/avatars/data";
 import mockData from "../../lib/mockData/notifications.json";
+import { convertToRelativeUnit } from "../../styles/helpers/convertToRelativeUnits";
 
 export const NotificationsPage = () => {
   const handleReview = () => {
@@ -9,148 +11,86 @@ export const NotificationsPage = () => {
   };
 
   return (
-    <Style.PageContainer>
-      <Style.Background />
-      <Style.TitleLine>
-        <Style.Title>Notifications</Style.Title>
-      </Style.TitleLine>
-      <Style.ScrollContainer>
+    <Scrollbar width="thick">
+      <Style.PageContainer>
+        <h2>Activity</h2>
         <Style.ListContainer>
-          <Style.SubTitle>Activity</Style.SubTitle>
-          <Style.NotificationsContainer>
-            {mockData.listData.map((notification, index) => (
-              <Style.Row key={index}>
-                <div className="icon">{avatars_data[0].icon()}</div>
-                <div className="text">
-                  <div className="role">{notification.role}</div>
-                  <p className="activity">{notification.content}</p>
-                </div>
-                <div className="date">{notification.date}</div>
-                <Button className="review" onClick={handleReview} size="small">
-                  <span style={{ padding: "0px 15px" }}>Review</span>
-                </Button>
-              </Style.Row>
-            ))}
-          </Style.NotificationsContainer>
+          {mockData.listData.map((notification, index) => (
+            <Style.Row key={index}>
+              <div className="icon">{avatars_data[index].icon()}</div>
+              <div className="text">
+                <div className="role">{notification.role}</div>
+                <p className="activity">{notification.content}</p>
+              </div>
+              <div className="date">{notification.date}</div>
+              <Button className="review" onClick={handleReview} size="small">
+                <span style={{ padding: "0px 15px" }}>Review</span>
+              </Button>
+            </Style.Row>
+          ))}
         </Style.ListContainer>
-      </Style.ScrollContainer>
-    </Style.PageContainer>
+      </Style.PageContainer>
+    </Scrollbar>
   );
 };
 
 const Style = {
   PageContainer: styled.div`
-    padding: 118px 70px 0 70px;
     display: flex;
-    height: 100vh;
     flex-direction: column;
-    position: relative;
-    overflow: overlay;
-    max-height: 100vh;
-    font-family: "Noir Std";
-    font-style: normal;
-  `,
-  Background: styled.div`
-    position: absolute;
-    top: 0;
-    left: -32px;
-    width: calc(100% + 32px);
-    height: 100vh;
-    background: linear-gradient(252.45deg, #c4e8ff 2.54%, #fff9e0 87.48%);
-    z-index: -1;
-  `,
-  TitleLine: styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    padding: 0px;
-    gap: 12px;
-    color: var(--orange-500);
-    padding-bottom: 38.53px;
+    height: 100%;
 
-    img: {
-      color: var(--orange-500);
+    h2 {
+      margin-bottom: ${convertToRelativeUnit(16, "vh")};
     }
   `,
-  Title: styled.p`
-    font-weight: 600;
-    font-size: 48px;
-    line-height: 56px;
-  `,
-  ListContainer: styled.div`
-    padding: 24px 32px 24px 32px;
+  ListContainer: styled.ul`
+    padding: ${convertToRelativeUnit(24, "vh")};
     background: rgba(255, 255, 255, 0.5);
     box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.1);
     backdrop-filter: blur(59.2764px);
     border-radius: 16px;
     display: flex;
-    gap: 20px;
     flex-direction: column;
-  `,
-  SubTitle: styled.p`
-    font-family: "Noir Std";
-    font-style: normal;
-    font-weight: 600;
-    font-size: 28px;
-    line-height: 32px;
-    color: var(--neutral-600);
-  `,
-  NotificationsContainer: styled.ul`
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    border-collapse: collapse;
-    font-family: "Noir Std";
-    font-style: normal;
-    list-style-type: none;
+    gap: ${convertToRelativeUnit(20, "vh")};
   `,
   Row: styled.li`
     width: 100%;
-    border-bottom: 1px solid black;
     border-bottom: 1px solid var(--neutral-600);
-    padding: 24px 0px;
     display: flex;
     align-items: center;
+    height: ${convertToRelativeUnit(128, "vh")};
 
     .icon {
-      padding-right: 32px;
-      font-weight: 600;
-      font-size: 23px;
-      line-height: 28px;
-      color: var(--neutral-700);
+      padding-right: ${convertToRelativeUnit(32, "vh")};
     }
+
     .text {
       flex-grow: 1;
       display: flex;
       flex-direction: column;
-      gap: 9px;
+      gap: ${convertToRelativeUnit(9, "vh")};
+
       .role {
         font-weight: 600;
-        font-size: 23px;
-        line-height: 28px;
+        font-size: ${convertToRelativeUnit(23, "vh")};
+        line-height: ${convertToRelativeUnit(28, "vh")};
         color: var(--neutral-600);
       }
       .activity {
         font-weight: 400;
-        font-size: 18px;
-        line-height: 24px;
+        font-size: ${convertToRelativeUnit(18, "vh")};
+        line-height: ${convertToRelativeUnit(24, "vh")};
         color: var(--neutral-600);
       }
     }
+
     .date {
-      width: 226px;
+      width: ${convertToRelativeUnit(226, "vw")};
       font-weight: 400;
-      font-size: 18px;
-      line-height: 24px;
+      font-size: ${convertToRelativeUnit(18, "vh")};
+      line-height: ${convertToRelativeUnit(24, "vh")};
       color: var(--neutral-700);
     }
-  `,
-  Text: styled.p``,
-  Date: styled.p``,
-  ScrollContainer: styled.div`
-    overflow-y: auto;
-    height: 100%;
-    padding-right: 16px;
-    margin-right: -24px;
   `,
 };
