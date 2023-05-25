3a9167099589aaf654f20083dd5450bb9fb5d849
AlexAlampour <aalampour@groovejones.com>
2023-05-25 08:10:17 -0500

    completed the scrollbar component

--- src/components/Global/Scrollbar/index.tsx
+++ src/components/Global/Scrollbar/index.tsx
@@ -1,22 +1,19 @@
 import React from "react";
 import styled from "styled-components";
-import { convertToRelativeUnit } from "../../../styles/helpers/convertToRelativeUnits";
 
 // Define the styled component
 const ScrollableDiv = styled.div<{ width: "thin" | "thick" }>`
-  width: 100%;
   height: 100%;
+  margin-right: ${(props) => (props.width === "thin" ? "-4px" : "-28px")};
+  padding-right: ${(props) => (props.width === "thin" ? "0px" : "20px")};
   overflow: auto;
   ::-webkit-scrollbar {
-    width: ${(props) =>
-      props.width === "thin"
-        ? convertToRelativeUnit(4, "vh")
-        : convertToRelativeUnit(8, "vh")};
+    width: ${(props) => (props.width === "thin" ? "4px" : "8px")};
   }
   ::-webkit-scrollbar-track {
   }
   ::-webkit-scrollbar-thumb {
-    background: var(--blue-500);
+    background: var(--blue-300);
     opacity: 0.5;
     border-radius: 100px;
   }
