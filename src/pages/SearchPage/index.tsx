a85444294d78a38a5ad7a8bce078d87436c46036
nmomoh <nobert@groovejones.com>
2023-06-02 20:28:03 +0100

    created empty look

--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -1,44 +1,40 @@
-import { useMemo } from "react";
 import styled from "styled-components";
 import { Typography } from "../../components/Typography";
+import { capitalize } from "../../lib/util/capitalize";
 import { useSearchResultsStore } from "../../stores/searchResultsStore";
 import { convertToRelativeUnit } from "../../styles/helpers/convertToRelativeUnits";
 
 export const SearchPage = () => {
-  const { searchKey, results } = useSearchResultsStore();
-  const count = useMemo(() => results.length, [results]);
+  const { results } = useSearchResultsStore();
+
+  // const count = useMemo(() => results.length, [results]);
+  const count = 0;
+  const searchKey: string | null | undefined = capitalize("dummy");
+  console.log("Search Key >>>>>>");
 
   return (
     <Style.PageContainer>
       <Typography
         tag="h1"
         color="orange-500"
-        size={convertToRelativeUnit(40, "vh")}
+        size="4vh"
         weight={600}
-      >{`Search Results for “${searchKey}”`}</Typography>
-      <p>{`Results ${count} of ${count} for ${searchKey}`} </p>
-      {results.map((result, index) => (
-        <Style.ListItem key={`list-${index}`}>
-          <img
-            src={result.image}
-            width="110px"
-            height="88px"
-            alt={result.title}
-          />
-          <div className="text-container">
-            <p className="title">{result.title}</p>
-            <p
-              className="content"
-              dangerouslySetInnerHTML={{
-                __html: result.content.replaceAll(
-                  searchKey,
-                  `<strong>${searchKey}</strong>`,
-                ),
-              }}
-            />
-          </div>
-        </Style.ListItem>
-      ))}
+        mb="1vh"
+      >{`Search Results for “${searchKey ?? "Dummy"}”`}</Typography>
+      <Typography tag="h2" size="3vh" weight={600} color="neutral-800">
+        {`${count} Results  for ${searchKey}`}{" "}
+      </Typography>
+
+      <div className="content-container">
+        <article className="empty-container">
+          <Typography color="neutral-400" size="3.5vh" weight={600} mb="1.5vh">
+            No results found
+          </Typography>
+          <Typography color="neutral-500" size="2vh" weight={500}>
+            There's nothing here. Try using different word
+          </Typography>
+        </article>
+      </div>
     </Style.PageContainer>
   );
 };
@@ -51,6 +47,21 @@ const Style = {
     flex-direction: column;
     padding: 9vh ${convertToRelativeUnit(32, "vw")} 2.5vh
       ${convertToRelativeUnit(64, "vw")};
+
+    .content-container {
+      width: 100%;
+      flex-grow: 1;
+      margin-top: ${convertToRelativeUnit(10, "vh")};
+
+      .empty-container {
+        height: 100%;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+      }
+    }
   `,
   ListItem: styled.div`
     display: flex;
