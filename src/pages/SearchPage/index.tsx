79f6090077d30edeb4515b54a618acf5a75ca3e0
nmomoh <nobert@groovejones.com>
2023-06-03 17:35:40 +0100

    search page design

--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -1,11 +1,13 @@
 import styled from "styled-components";
+import Scrollable from "../../components/Global/Scrollable";
+import { SearchItem } from "../../components/Search/SearchItem";
 import { Typography } from "../../components/Typography";
 import { capitalize } from "../../lib/util/capitalize";
-import { useSearchResultsStore } from "../../stores/searchResultsStore";
+import { animatedbackgroundGradient } from "../../styles/helpers/animatedBackgroundGradient";
 import { convertToRelativeUnit } from "../../styles/helpers/convertToRelativeUnits";
 
 export const SearchPage = () => {
-  const { results } = useSearchResultsStore();
+  // const { results } = useSearchResultsStore();
 
   // const count = useMemo(() => results.length, [results]);
   const count = 0;
@@ -25,16 +27,20 @@ export const SearchPage = () => {
         {`${count} Results  for ${searchKey}`}{" "}
       </Typography>
 
-      <div className="content-container">
-        <article className="empty-container">
+      <Scrollable height="auto" className="content-container">
+        {/* <article className="empty-container">
           <Typography color="neutral-400" size="3.5vh" weight={600} mb="1.5vh">
             No results found
           </Typography>
           <Typography color="neutral-500" size="2vh" mb="7.5vh" weight={500}>
             There's nothing here. Try using different word
           </Typography>
-        </article>
-      </div>
+        </article> */}
+        <SearchItem />
+        <SearchItem />
+        <SearchItem />
+        <SearchItem />
+      </Scrollable>
     </Style.Container>
   );
 };
@@ -47,11 +53,15 @@ const Style = {
     flex-direction: column;
     padding: 9vh ${convertToRelativeUnit(32, "vw")} 2.5vh
       ${convertToRelativeUnit(64, "vw")};
+    ${() => animatedbackgroundGradient("#C4E8FF", "#FFF9E0")}
 
     .content-container {
       width: 100%;
-      flex-grow: 1;
       margin-top: ${convertToRelativeUnit(10, "vh")};
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      align-items: flex-start;
 
       .empty-container {
         height: 100%;
