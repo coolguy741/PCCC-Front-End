88d328952a354a60d49f24817735e927fdba7842
nmomoh <nobert@groovejones.com>
2023-06-06 21:04:11 +0100

    initial theme enum

---
+++ src/components/Theme/types.ts
@@ -0,0 +1,10 @@
+export enum ThemeComponents {
+  Theme = "theme",
+  ParagraphWithHeading = "paragraph-with-heading",
+  SingleImage = "single-image",
+  DoubleImage = "double-image",
+  AssessmentMultipleChoice = "assessment-multiple-choice",
+  AssessmentTrueOrFalse = "assessment-true-or-false",
+  AssessmentWritten = "assessment-written",
+  Title = "title",
+}
