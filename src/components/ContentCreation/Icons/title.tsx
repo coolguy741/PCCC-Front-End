8a7baaa0abc42f065a043e15c6f598bd03ffc4db
nmomoh <nobert@groovejones.com>
2023-06-11 21:09:07 +0100

    pushed initial menu

--- src/components/ContentCreation/Icons/title.tsx
+++ src/components/ContentCreation/Icons/title.tsx
@@ -3,20 +3,19 @@ import { IconProps } from "../../../types/icon";
 export default function TitleIcon(props: IconProps) {
   return (
     <svg
-      width="172"
-      height="70"
-      viewBox="0 0 172 70"
+      width="169"
+      height="80"
+      viewBox="0 0 169 80"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <rect width="33" height="10" rx="5" fill="#11A5FF" />
-      <path d="M0 24H45" stroke="#88D2FF" stroke-width="8" />
-      <path d="M0 36H172" stroke="#88D2FF" stroke-width="3" />
-      <path d="M0 52H172" stroke="#88D2FF" stroke-width="3" />
-      <path d="M0 68H172" stroke="#88D2FF" stroke-width="3" />
-      <path d="M0 44H172" stroke="#88D2FF" stroke-width="3" />
-      <path d="M0 60H172" stroke="#88D2FF" stroke-width="3" />
+      <path d="M6 69H46" stroke="#11A5FF" stroke-width="3" />
+      <path d="M6 38H46" stroke="#11A5FF" stroke-width="3" />
+      <path d="M6 49H46" stroke="#11A5FF" stroke-width="3" />
+      <path d="M6 59H46" stroke="#11A5FF" stroke-width="3" />
+      <rect x="55" width="114" height="80" fill="#88D2FF" />
+      <rect y="11" width="65" height="18" fill="#11A5FF" />
     </svg>
   );
 }
--- src/pages/TestContent/index.tsx
+++ src/pages/TestContent/index.tsx
@@ -1,10 +1,39 @@
 import styled from "styled-components";
 import { DoubleBullet } from "../../components/ContentCreation/DoubleBullet";
+import DoubleImageIcon from "../../components/ContentCreation/Icons/doubleImage";
+import PWH from "../../components/ContentCreation/Icons/pwh";
+import PWN from "../../components/ContentCreation/Icons/pwn";
+import SingleImageIcon from "../../components/ContentCreation/Icons/singleImage";
+import TitleIcon from "../../components/ContentCreation/Icons/title";
 
 export const TestContentPage = () => {
   return (
     <Style.PageContainer>
-      <div className="cc-components"></div>
+      <div className="cc-components">
+        <button>
+          <p>Title card</p>
+          <TitleIcon />
+        </button>
+        <button>
+          <p>Paragraph</p>
+          <PWH />
+        </button>
+        <button>
+          <p>
+            Paragraph with
+            <br /> Number
+          </p>
+          <PWN />
+        </button>
+        <button>
+          <p>1x1 Image</p>
+          <SingleImageIcon />
+        </button>
+        <button>
+          <p>1x2 Image</p>
+          <DoubleImageIcon />
+        </button>
+      </div>
       <div className="cc-preview">
         {/* <ThemeComponent /> */}
         {/* <Title /> */}
@@ -31,6 +60,29 @@ const Style = {
       height: 100%;
       width: 10%;
       border: 0.5px solid rgba(0, 0, 0, 0.1);
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      button {
+        width: 90%;
+        padding: 1vh;
+        margin: 0.5vh 0;
+
+        p {
+          width: max-content;
+          font-weight: 400;
+          font-size: 14px;
+          line-height: 20px;
+          color: #3d3d3d;
+          margin-bottom: 1vh;
+        }
+      }
+
+      svg {
+        width: 75%;
+        margin-bottom: 2vh;
+      }
     }
 
     .cc-preview {
