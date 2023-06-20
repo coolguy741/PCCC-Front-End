9ef07e7200386ea4d31637e3fc781a03517eaa1b
nmomoh <nobert@groovejones.com>
2023-06-20 09:23:07 +0100

    added image component

--- src/components/ContentCreation/DoubleImage/index.tsx
+++ src/components/ContentCreation/DoubleImage/index.tsx
@@ -1,19 +1,16 @@
 import styled from "styled-components";
+import { Image } from "../Image/image";
 
 export function DoubleImage() {
   return (
-    <Style.Container img="/public/content-creation/img-pattern.png">
-      <figure>
-        <img alt="" />
-        <figcaption></figcaption>
-      </figure>
+    <Style.Container>
+      <Image img="" />
     </Style.Container>
   );
 }
 
 const Style = {
-  Container: styled.article<{ img: string }>`
-    background: ${({ img }) => `url(${img})`};
+  Container: styled.article`
     background-position: center;
     background-size: cover;
     box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.1);
@@ -21,6 +18,6 @@ const Style = {
     border-radius: 16px;
     width: 100%;
     height: 100%;
-    padding: 2.5vh 2vw;
+    overflow: hidden;
   `,
 };
