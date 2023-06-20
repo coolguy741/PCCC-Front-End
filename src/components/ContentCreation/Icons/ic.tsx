6548e36131c4dcd450044884331eb0958def7fe3
nmomoh <nobert@groovejones.com>
2023-06-20 11:16:51 +0100

    added icons, other components

---
+++ src/components/ContentCreation/Icons/ic.tsx
@@ -0,0 +1,26 @@
+import { IconProps } from "../../../types/icon";
+
+export default function IC(props: IconProps) {
+  return (
+    <svg
+      width="169"
+      height="65"
+      viewBox="0 0 169 65"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M0 3H71" stroke="#11A5FF" stroke-width="6" />
+      <path d="M0 20H31" stroke="#88D2FF" stroke-width="3" />
+      <path d="M51 20H169" stroke="#88D2FF" stroke-width="3" />
+      <path d="M0 32H31" stroke="#88D2FF" stroke-width="3" />
+      <path d="M51 32H169" stroke="#88D2FF" stroke-width="3" />
+      <path d="M0 43H31" stroke="#88D2FF" stroke-width="3" />
+      <path d="M51 43H169" stroke="#88D2FF" stroke-width="3" />
+      <path d="M0 53H31" stroke="#88D2FF" stroke-width="3" />
+      <path d="M51 53H169" stroke="#88D2FF" stroke-width="3" />
+      <path d="M0 63H31" stroke="#88D2FF" stroke-width="3" />
+      <path d="M51 63H169" stroke="#88D2FF" stroke-width="3" />
+    </svg>
+  );
+}
--- src/pages/TestContent/index.tsx
+++ src/pages/TestContent/index.tsx
@@ -1,73 +1,124 @@
+import _ from "lodash";
 import { useState } from "react";
 import styled from "styled-components";
+import { DoubleBullet } from "../../components/ContentCreation/DoubleBullet";
 import { DoubleImage } from "../../components/ContentCreation/DoubleImage";
+import DB from "../../components/ContentCreation/Icons/db";
 import DoubleImageIcon from "../../components/ContentCreation/Icons/doubleImage";
+import IC from "../../components/ContentCreation/Icons/ic";
+import IWC from "../../components/ContentCreation/Icons/iwc";
 import PWH from "../../components/ContentCreation/Icons/pwh";
 import PWN from "../../components/ContentCreation/Icons/pwn";
+import SB from "../../components/ContentCreation/Icons/sb";
 import SingleImageIcon from "../../components/ContentCreation/Icons/singleImage";
 import TitleIcon from "../../components/ContentCreation/Icons/title";
+import { ImageWithCaption } from "../../components/ContentCreation/ImageWithCaption";
+import { IngredientCard } from "../../components/ContentCreation/IngredientCard";
 import { NumberedParagraph } from "../../components/ContentCreation/NumberedParagraph";
 import { ParagraphWithHeading } from "../../components/ContentCreation/ParagraphWithHeading";
+import { SingleBullet } from "../../components/ContentCreation/SingleBullet";
 import { SingleImage } from "../../components/ContentCreation/SingleImage";
 import { Title } from "../../components/ContentCreation/Title";
+import Scrollable from "../../components/Global/Scrollable";
 
-const contentComponents = {
-  title: <Title key="title" />,
-  pwh: <ParagraphWithHeading key="pwh" />,
-  np: <NumberedParagraph key="np" />,
-  si: <SingleImage key="si" />,
-  di: <DoubleImage key="di" />,
-};
-
-type Comp = "pwh" | "title" | "np" | "si" | "di";
+const components = [
+  {
+    id: 1,
+    title: "Title Card",
+    preview: <TitleIcon />,
+    component: <Title key="title" />,
+  },
+  {
+    id: 2,
+    title: "Paragraph",
+    preview: <PWH />,
+    component: <ParagraphWithHeading key="pwh" />,
+  },
+  {
+    id: 3,
+    title: "Paragraph with Number",
+    preview: <PWN />,
+    component: <NumberedParagraph key="np" />,
+  },
+  {
+    id: 4,
+    title: "1X1 Image",
+    preview: <SingleImageIcon />,
+    component: <SingleImage key="si" />,
+  },
+  {
+    id: 5,
+    title: "1x2 Image",
+    preview: <DoubleImageIcon />,
+    component: <DoubleImage key="di" />,
+  },
+  {
+    id: 6,
+    title: "Assessment - Multiple Choice",
+    preview: <TitleIcon />,
+    component: <Title key="title" />,
+  },
+  {
+    id: 7,
+    title: "Assessment - True & False",
+    preview: <PWH />,
+    component: <ParagraphWithHeading key="pwh" />,
+  },
+  {
+    id: 8,
+    title: "Assessment - Written",
+    preview: <PWN />,
+    component: <NumberedParagraph key="np" />,
+  },
+  {
+    id: 9,
+    title: "Image with Caption",
+    preview: <IWC />,
+    component: <ImageWithCaption key="iwc" />,
+  },
+  {
+    id: 10,
+    title: "Bullet 1",
+    preview: <SB />,
+    component: <SingleBullet />,
+  },
+  {
+    id: 10,
+    title: "Ingredient Card",
+    preview: <IC />,
+    component: <IngredientCard />,
+  },
+  {
+    id: 11,
+    title: "Bullet 2",
+    preview: <DB />,
+    component: <DoubleBullet />,
+  },
+];
 
 export const TestContentPage = () => {
-  const [comp, setComp] = useState<Comp>("title");
+  const [comp, setComp] = useState<string>("Title Card");
 
-  function changeComp(newState: Comp) {
+  function changeComp(newState: string) {
     if (comp !== newState) setComp(newState);
+  }
+
+  function getComp(state: string) {
+    const comp = _.find(components, { title: state });
+    return comp?.component;
   }
 
   return (
     <Style.PageContainer>
-      <div className="cc-components">
-        <button onClick={() => changeComp("title")}>
-          <p>Title card</p>
-          <TitleIcon />
-        </button>
-        <button onClick={() => changeComp("pwh")}>
-          <p>Paragraph</p>
-          <PWH />
-        </button>
-        <button onClick={() => changeComp("np")}>
-          <p>
-            Paragraph with
-            <br /> Number
-          </p>
-          <PWN />
-        </button>
-        <button onClick={() => changeComp("si")}>
-          <p>1x1 Image</p>
-          <SingleImageIcon />
-        </button>
-        <button onClick={() => changeComp("di")}>
-          <p>1x2 Image</p>
-          <DoubleImageIcon />
-        </button>
-      </div>
-      <div className="cc-preview">
-        {contentComponents[comp]}
-        {/* <ThemeComponent /> */}
-        {/* <Title /> */}
-        {/* <ParagraphWithHeading /> */}
-        {/* <SingleImage /> */}
-        {/* <DoubleImage /> */}
-        {/* <NumberedParagraph /> */}
-        {/* <ImageWithCaption /> */}
-        {/* <SingleBullet /> */}
-        {/* <IngredientCard /> */}
-        {/* <DoubleBullet /> */}
-      </div>
+      <Scrollable className="cc-components">
+        {components.map(({ preview, title }) => (
+          <button key={title} onClick={() => changeComp(title)}>
+            <p>{title}</p>
+            <div className="btn-icon">{preview}</div>
+          </button>
+        ))}
+      </Scrollable>
+      <div className="cc-preview">{getComp(comp)}</div>
     </Style.PageContainer>
   );
 };
@@ -80,36 +131,47 @@ const Style = {
 
     .cc-components {
       height: 100%;
-      width: 10%;
+      width: 15%;
       border: 0.5px solid rgba(0, 0, 0, 0.1);
       display: flex;
       flex-direction: column;
       align-items: center;
 
       button {
+        display: flex;
+        flex-direction: column;
+        margin: 30px 0;
         width: 90%;
-        padding: 1vh;
-        margin: 0.5vh 0;
 
         p {
-          width: max-content;
           font-weight: 400;
-          font-size: 14px;
+          font-size: 12px;
           line-height: 20px;
+          text-align: center;
           color: #3d3d3d;
-          margin-bottom: 1vh;
+          margin-bottom: 7.5px;
         }
-      }
 
-      svg {
-        width: 75%;
-        margin-bottom: 2vh;
+        div {
+          height: 100px;
+          width: 100%;
+          background: rgba(255, 255, 255, 0.5);
+          box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.1);
+          backdrop-filter: blur(59.2764px);
+          border-radius: 8px;
+          display: grid;
+          place-items: center;
+
+          svg {
+            height: 50%;
+          }
+        }
       }
     }
 
     .cc-preview {
       height: 100%;
-      width: 90%;
+      width: 85%;
       padding: 40px;
       display: grid;
       place-items: center;
