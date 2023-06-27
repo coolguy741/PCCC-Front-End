4168ca759c1f10892871a53f4daf3c479edd58cc
nmomoh <nobert@groovejones.com>
2023-06-27 12:58:22 +0100

    added icons, modifying look

--- src/pages/CloudDrivePage/index.tsx
+++ src/pages/CloudDrivePage/index.tsx
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 import { CDHeader } from "../../components/CloudDrive/Header";
+import CDAdd from "../../components/CloudDrive/Icons/cd-add";
+import CDGallery from "../../components/CloudDrive/Icons/cd-gallery";
+import CDList from "../../components/CloudDrive/Icons/cd-list";
 import { Typography } from "../../components/Typography";
+import { convertToRelativeUnit } from "../../styles/helpers/convertToRelativeUnits";
 import { glassBackground } from "../../styles/helpers/glassBackground";
 // import { Icon } from "../../components/Global/Icon";
 // import SharingIcon from "../../components/Icons/Sharing/sharing";
@@ -148,7 +152,24 @@ export const CloudDrivePage = () => {
             >
               Files
             </Typography>
-            <div></div>
+            <div>
+              <button className="cdf-view-option active">
+                <CDGallery />
+              </button>
+              <button className="cdf-view-option">
+                <CDList />
+              </button>
+              <button className="cdf-upload">
+                <CDAdd />
+                <Typography
+                  color="white"
+                  weight={600}
+                  size={convertToRelativeUnit(18, "vh")}
+                >
+                  Upload
+                </Typography>
+              </button>
+            </div>
           </div>
           <article className="cd-content"></article>
         </div>
@@ -190,12 +211,56 @@ const Style = {
       .cdf-menu-options {
         height: 5vh;
         width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin-bottom: 2.5vh;
+
+        div {
+          display: flex;
+          height: 100%;
+          width: 25%;
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+
+          button.cdf-view-option {
+            border-radius: 50%;
+            aspect-ratio: 1 / 1;
+            height: 100%;
+            display: grid;
+            place-items: center;
+          }
+
+          button.active {
+            background: #fff;
+            box-shadow: 0px 4px 16px 0px rgba(0, 0, 0, 0.1);
+          }
+
+          button.cdf-upload {
+            display: flex;
+            padding: 10px 20px;
+            justify-content: center;
+            align-items: center;
+            background: var(
+              --gradient-orange,
+              linear-gradient(180deg, #f87c56 0%, #f65321 100%)
+            );
+            box-shadow: 0px 4px 5px 0px rgba(248, 124, 86, 0.4);
+            border-radius: 80px;
+
+            svg {
+              margin-right: 9px;
+            }
+          }
+        }
       }
 
       article.cd-content {
         flex-grow: 1;
         border-radius: 16px;
         ${glassBackground};
+        padding: ${convertToRelativeUnit(24, "vh")};
       }
     }
   `,
