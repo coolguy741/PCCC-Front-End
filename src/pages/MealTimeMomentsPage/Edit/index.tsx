e133e38d13a53efbf404d361ad95c8fc37a0fda1
coolguy741 <webreplies2022@gmail.com>
2023-06-29 14:33:16 -0600

    Edit mealtime moments page

--- src/pages/MealTimeMomentsPage/Edit/index.tsx
+++ src/pages/MealTimeMomentsPage/Edit/index.tsx
@@ -1,151 +1,156 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
 import styled from "styled-components";
-import { Button } from "../../../components/Global/Button";
-import { AlignmentStyle } from "../../../components/Global/Container";
-import { FileUploader } from "../../../components/Global/FileUploader";
-import { LanguageChooser } from "../../../components/Global/LanguageChooser";
-import { ModalContainer } from "../../../components/Global/ModalContainer";
-import { SaveChangesModal } from "../../../components/Global/SaveChangesModal";
-import { SmallButton } from "../../../components/Global/SmallButton";
-import mockData from "../../../lib/mockData/mealtime-moments/mealtime-moment-edit.json";
+import Button from "../../../components/Button";
+import { LanguageToggle } from "../../../components/ContentBuilder/Components/ContentInfo/LangToggle";
+import { Tags } from "../../../components/ContentBuilder/Components/ContentInfo/Tag";
+import { MedaltimeMomentTitle } from "../../../components/ContentCreation/MealtimeMomentTitle";
+import { BackButton } from "../../../components/Global/BackButton";
+import { Typography } from "../../../components/Global/Typography";
+
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/scrollbar";
 
 export const EditMealtimeMomentPage = () => {
-  const [lang, setLang] = useState<string>(
-    localStorage.getItem("lang") ?? "en",
-  );
-  const [titleEn, setTitleEn] = useState(mockData.en.title);
-  const [titleFr, setTitleFr] = useState(mockData.fr.title);
-  const [overivewEn, setOverviewEn] = useState(mockData.en.overview);
-  const [overivewFr, setOverviewFr] = useState(mockData.fr.overview);
-  const [visibleModal, setVisibleModal] = useState(false);
-
   const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [tags, setTags] = useState(["foraging", "seeds"]);
 
-  const handleBack = () => {
-    navigate(-1);
+  const addTag = (tag: string) => {
+    setTags((prev) => [...prev, tag]);
   };
 
-  const handleSave = () => {
-    alert("Save changes");
+  const deleteTag = (tag: string) => {
+    setTags((prev) => [...prev.filter((current) => current !== tag)]);
   };
 
-  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    if (lang === "en") {
-      setTitleEn(value);
-    } else {
-      setTitleFr(value);
-    }
+  const handleSaveAndContinue = () => {
+    console.log("Save and continue");
   };
 
-  const handleOverviewChange = (
-    event: React.ChangeEvent<HTMLTextAreaElement>,
-  ) => {
-    const value = event.target.value;
-    if (lang === "en") {
-      setOverviewEn(value);
-    } else {
-      setOverviewFr(value);
-    }
-  };
-
-  const handleToggleLanguage = () => {
-    setVisibleModal(false);
-    if (lang === "en") setLang("fr");
-    else setLang("en");
+  const handleCreate = () => {
+    console.log("Save and exit");
   };
 
   return (
-    <Style.PageContainer>
-      <AlignmentStyle.LeftAlignedContainer>
-        <Button onClick={handleBack}>Back</Button>
-      </AlignmentStyle.LeftAlignedContainer>
-      <AlignmentStyle.CenterAlignedContainer>
-        <LanguageChooser lang={lang} setLang={setLang} />
-      </AlignmentStyle.CenterAlignedContainer>
-      <Style.Content>
-        <Style.DetailContainer>
-          <AlignmentStyle.LeftAlignedContainer>
-            <h3>Title</h3>
-          </AlignmentStyle.LeftAlignedContainer>
-          <Style.TitleInput
-            value={lang === "en" ? titleEn : titleFr}
-            onChange={handleTitleChange}
-          />
-        </Style.DetailContainer>
-        <Style.OverviewImageContainer>
-          <Style.DetailContainer>
-            <h3>Overview</h3>
-            <Style.OverviewTextArea
-              value={lang === "en" ? overivewEn : overivewFr}
-              rows={15}
-              onChange={handleOverviewChange}
-            ></Style.OverviewTextArea>
-          </Style.DetailContainer>
-          <Style.DetailContainer>
-            <h3>Images and Videos</h3>
-            <FileUploader />
-          </Style.DetailContainer>
-        </Style.OverviewImageContainer>
-      </Style.Content>
-      <AlignmentStyle.RightAlignedContainer>
-        <SmallButton onClick={() => setVisibleModal(true)}>
-          Save changes
-        </SmallButton>
-      </AlignmentStyle.RightAlignedContainer>
-      {visibleModal && (
-        <ModalContainer>
-          <SaveChangesModal
-            onSave={handleSave}
-            onToggleLanguage={handleToggleLanguage}
-          />
-        </ModalContainer>
-      )}
-    </Style.PageContainer>
+    <Style.Container>
+      <div className="buttons-container">
+        <BackButton onClick={() => navigate("/dashboard/mealtime-moments")} />
+        <Button>Preview</Button>
+      </div>
+
+      <Typography variant="h4" as="h4" weight="semi-bold">
+        Edit mealtime moment
+      </Typography>
+
+      <Style.Info>
+        <div className="flex">
+          <LanguageToggle />
+        </div>
+        <div className="flex">
+          <Tags tags={tags} addTag={addTag} deleteTag={deleteTag} />
+        </div>
+      </Style.Info>
+      <Style.ContentBuilder>
+        <MedaltimeMomentTitle
+          setTitle={setTitle}
+          setDescription={setDescription}
+        />
+      </Style.ContentBuilder>
+      <Style.ActionContainer>
+        <div className="flex">
+          <Button variant="yellow" className="mr-4" onClick={handleCreate}>
+            Save changes and exit
+          </Button>
+          <Button variant="orange" onClick={handleSaveAndContinue}>
+            Save changes and continue
+          </Button>
+        </div>
+      </Style.ActionContainer>
+    </Style.Container>
   );
 };
 
 const Style = {
-  PageContainer: styled.div`
+  Container: styled.section`
+    display: flex;
+    overflow: hidden;
+    flex-direction: column;
+    height: 100vh;
+
+    .buttons-container {
+      display: flex;
+      justify-content: space-between;
+      padding: 0 0 0.5rem;
+      height: 6rem;
+
+      button {
+        margin: 1.2vh 0;
+      }
+    }
+  `,
+  Info: styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    align-items: center;
+    padding: 2rem 0 1rem;
+
+    span {
+      color: var(--blue-500);
+    }
+  `,
+  SlideDeleteButton: styled.button`
+    display: flex;
+    gap: 10%;
+    align-items: center;
+    background: rgba(255, 255, 255, 0.75);
+    margin-right: 2rem;
+    padding: 0.1vh 1vw;
+    border-radius: 0.5rem;
+    width: 100%;
+
+    * {
+      white-space: nowrap;
+    }
+  `,
+  Language: styled.div`
+    display: flex;
+  `,
+  ContentBuilder: styled.div`
+    background: rgba(255, 255, 255, 0.5);
+    box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(59.2764px);
+    border-radius: 16px;
+    overflow: hidden;
+    width: 100%;
+    display: flex;
+    height: 100%;
+    padding: 1rem;
+  `,
+  ActionContainer: styled.section`
+    padding: 2vh 0;
+    display: flex;
+    justify-content: flex-end;
+  `,
+  Slide: styled.section`
     display: flex;
     flex-direction: column;
-    gap: 30px;
+    width: 100%;
+
+    .theme-swiper-slide {
+      flex: 1;
+      margin: 0;
+    }
   `,
   Content: styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-  `,
-  TitleInput: styled.input`
-    border: solid 1px black;
-
-    &:focus {
-      outline: none;
-      border-color: #007bff;
-      box-shadow: 0 0 0 2px #007bff33;
-    }
-  `,
-  DetailContainer: styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    flex-basis: 0;
-    flex-grow: 1;
-  `,
-  OverviewImageContainer: styled.div`
-    display: flex;
-    justify-content: space-between;
+    height: 100%;
     width: 100%;
-    gap: 30px;
-  `,
-  OverviewTextArea: styled.textarea`
-    width: 100%;
-
-    &:focus {
-      outline: none;
-      border-color: #007bff;
-      box-shadow: 0 0 0 2px #007bff33;
-    }
+    padding: 1.87vh 1vw;
+    position: relative;
+    background: #ffffff50;
+    border-radius: 0.5rem;
   `,
 };
