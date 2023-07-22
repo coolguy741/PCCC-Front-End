ad29de50aae5863091b768cdb2acdc111a23d017
nmomoh <nobert@groovejones.com>
2023-07-22 21:52:48 +0100

    initial page setup

--- src/pages/AchievementsPage/index.tsx
+++ src/pages/AchievementsPage/index.tsx
@@ -1,38 +1,8 @@
 import styled from "styled-components";
-import { Achievement } from "../../components/Achievement";
-import mockData from "../../lib/mockData/achievements.json";
+import { PageTitleLayout } from "../../components/Global/PageTitleLayout";
 
 export const AchievementsPage = () => {
-  return (
-    <Style.PageContainer>
-      <h1>Achievement</h1>
-      <Style.SubContainer>
-        <h3>Bagdes</h3>
-        <Style.AchievementsContainer>
-          {mockData.badges.map((achievement, index) => (
-            <Achievement
-              key={index}
-              title={achievement.title}
-              content={achievement.content}
-            />
-          ))}
-        </Style.AchievementsContainer>
-      </Style.SubContainer>
-      <Style.SubContainer>
-        <h3>In progress</h3>
-        <Style.AchievementsContainer>
-          {mockData.inProgress.map((achievement, index) => (
-            <Achievement
-              key={index}
-              title={achievement.title}
-              content={achievement.content}
-              progressValue={achievement.progressValue}
-            />
-          ))}
-        </Style.AchievementsContainer>
-      </Style.SubContainer>
-    </Style.PageContainer>
-  );
+  return <PageTitleLayout title="Your profile" />;
 };
 
 const Style = {
