67b71458ad8ff3ee7d7da9ce3158a352e9b7f741
nmomoh <nobert@groovejones.com>
2023-07-23 11:38:38 +0100

    initial achievement functionality

--- src/pages/AchievementsPage/index.tsx
+++ src/pages/AchievementsPage/index.tsx
@@ -1,24 +1,35 @@
 import styled from "styled-components";
-import { PageTitleLayout } from "../../components/Global/PageTitleLayout";
+import { animatedbackgroundGradient } from "../../styles/helpers/animatedBackgroundGradient";
 
 export const AchievementsPage = () => {
-  return <PageTitleLayout title="Your profile" />;
+  return <Style.PageContainer />;
 };
 
 const Style = {
   PageContainer: styled.div`
+    padding: 7vh 32px 2.5vh 64px;
     display: flex;
+    height: 100vh;
     flex-direction: column;
-    gap: 30px;
-  `,
-  SubContainer: styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-  `,
-  AchievementsContainer: styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    gap: 20px;
+    position: relative;
+    max-height: 100vh;
+    font-family: "Noir Std";
+    font-style: normal;
+    ${() => animatedbackgroundGradient("var(--blue-200)", "#fff9e0")};
+
+    hgroup {
+      display: flex;
+      align-items: center;
+      gap: 0.5vw;
+      margin-bottom: 0.5vh;
+    }
+
+    h1 {
+      color: var(--orange-500);
+
+      img {
+        height: 3.5vh;
+      }
+    }
   `,
 };
