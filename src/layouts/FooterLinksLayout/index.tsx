4b41cbd62e0e98c2191b16af3e6c26ae14eff5c7
nmomoh <nobert@groovejones.com>
2023-07-25 09:31:54 +0100

    finished fl header look

--- src/layouts/FooterLinksLayout/index.tsx
+++ src/layouts/FooterLinksLayout/index.tsx
@@ -1,8 +1,7 @@
-import { motion } from "framer-motion";
 import styled from "styled-components";
-import { animatedbackgroundGradient } from "../../styles/helpers/animatedBackgroundGradient";
-import { convertToRelativeUnit as conv } from "../../styles/helpers/convertToRelativeUnits";
+import { convertToRelativeUnit } from "../../styles/helpers/convertToRelativeUnits";
 import { Header } from "../shared/Header/header";
+import { SideMenu } from "../shared/SideMenu/sideMenu";
 import { FLHeader } from "./header";
 
 interface Props {
@@ -11,25 +10,28 @@ interface Props {
 
 export function FooterLinksLayout({ children }: Props) {
   return (
-    <Style.Container layout layoutRoot>
+    <Style.Container className="dashboard-layout">
+      <SideMenu />
       <Header />
-      <FLHeader />
+      <main>
+        <FLHeader />
+        {children}
+      </main>
     </Style.Container>
   );
 }
 
 const Style = {
-  Container: styled(motion.div)`
+  Container: styled.div`
     width: 100%;
-    height: 100%;
-    display: grid;
-    place-items: center;
-    position: relative;
-    padding: ${conv(32, "vh")} ${conv(32, "vw")};
-    padding-top: ${conv(108, "vh")};
-    perspective: 1000px;
-    z-index: 0;
 
-    ${() => animatedbackgroundGradient("var(--blue-200)", "#fff9e0")}
+    main {
+      padding: 10vh ${convertToRelativeUnit(32, "vw")} 2.5vh
+        ${convertToRelativeUnit(64, "vw")};
+      height: 100%;
+      margin-left: calc(
+        var(--dashboard-menu-width-medium) - ${convertToRelativeUnit(32, "vw")}
+      );
+    }
   `,
 };
