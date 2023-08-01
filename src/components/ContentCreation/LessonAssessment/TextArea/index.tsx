d0919562585fac674123f57fe7f3a8fd6adad747
nmomoh <nobert@groovejones.com>
2023-08-01 13:19:02 +0100

    lesson assessment new look

--- src/components/ContentCreation/LessonAssessment/TextArea/index.tsx
+++ src/components/ContentCreation/LessonAssessment/TextArea/index.tsx
@@ -11,6 +11,14 @@ const taInitialState = {
     src: "",
     patternChoice: "",
   },
+  overview: {
+    mode: "view",
+    text: "Garden Guardian",
+  },
+  lessonName: {
+    mode: "view",
+    text: "Tools of the Trade - Assessment",
+  },
   number: {
     mode: "view",
     text: "Question 1 of 1",
@@ -36,50 +44,72 @@ export const LessonAssessmentTextAreaComponent = ({
 }: ComponentProps) => {
   return (
     <Style.Container>
-      <article className="lata-image">
-        <Media
-          changePattern={changeMediaPattern}
-          changeMediaState={changeMediaState}
-          media={state.media}
-        />
-      </article>
-      <article className="lata-content">
-        <hgroup>
-          <Typography
-            tag="h2"
-            color="blue-500"
-            size={conv(18, "vh")}
-            weight={500}
-            mb="1.5vh"
-          >
-            <DoubleClickToEditComponent
-              mode={state.number.mode}
-              setText={changeText}
-              changeEditState={changeEditState}
-              text={state.number.text}
-              name="number"
-            />
-          </Typography>
-          <Typography
-            tag="h3"
-            color="neutral-600"
-            size={conv(19, "vh")}
-            weight={600}
-            mb="5vh"
-          >
-            <DoubleClickToEditComponent
-              mode={state.question.mode}
-              setText={changeText}
-              changeEditState={changeEditState}
-              text={state.question.text}
-              name="question"
-            />
-          </Typography>
-        </hgroup>
-        <div className="lata-content-input">
-          <textarea className="lata-textarea" />
-        </div>
-      </article>
+      <hgroup className="la-top">
+        <h2>
+          <DoubleClickToEditComponent
+            mode={state.overview.mode}
+            setText={changeText}
+            changeEditState={changeEditState}
+            text={state.overview.text}
+            name="overview"
+          />
+        </h2>
+        <Typography tag="h3" size="3.5vh" color="neutral-700">
+          <DoubleClickToEditComponent
+            mode={state.lessonName.mode}
+            setText={changeText}
+            changeEditState={changeEditState}
+            text={state.lessonName.text}
+            name="lessonName"
+          />
+        </Typography>
+      </hgroup>
+      <div className="la-bottom">
+        <article className="lata-image">
+          <Media
+            changePattern={changeMediaPattern}
+            changeMediaState={changeMediaState}
+            media={state.media}
+          />
+        </article>
+        <article className="lata-content">
+          <hgroup>
+            <Typography
+              tag="h4"
+              color="blue-500"
+              size={conv(18, "vh")}
+              weight={500}
+              mb="1.5vh"
+            >
+              <DoubleClickToEditComponent
+                mode={state.number.mode}
+                setText={changeText}
+                changeEditState={changeEditState}
+                text={state.number.text}
+                name="number"
+              />
+            </Typography>
+            <Typography
+              tag="h5"
+              color="neutral-600"
+              size={conv(19, "vh")}
+              weight={600}
+              mb="5vh"
+            >
+              <DoubleClickToEditComponent
+                mode={state.question.mode}
+                setText={changeText}
+                changeEditState={changeEditState}
+                text={state.question.text}
+                name="question"
+              />
+            </Typography>
+          </hgroup>
+          <div className="lata-content-input">
+            <textarea className="lata-textarea" />
+          </div>
+        </article>
+      </div>
     </Style.Container>
   );
 };
@@ -93,12 +123,36 @@ const Style = {
   Container: styled.section`
     width: 100%;
     height: 100%;
-    border-radius: ${conv(32, "vh")};
-    filter: drop-shadow(0px 4px 16px rgba(0, 0, 0, 0.1));
-    backdrop-filter: blur(59.2764px);
     display: flex;
-    background: rgba(255, 255, 255, 0.5);
-    overflow: hidden;
+    flex-direction: column;
+
+    .la-top {
+      width: 100%;
+      height: 15%;
+
+      h2 {
+        font-weight: 600;
+        font-size: ${conv(14, "vh")};
+        text-transform: uppercase;
+        color: var(--orange-600);
+        border: 2px solid var(--orange-600);
+        padding: ${conv(10, "vh")} ${conv(30, "vw")};
+        border-radius: 24px;
+        width: max-content;
+        margin-bottom: 10px;
+      }
+    }
+
+    .la-bottom {
+      width: 100%;
+      height: 82.5%;
+      display: flex;
+      background: rgba(255, 255, 255, 0.5);
+      filter: drop-shadow(0px 4px 16px rgba(0, 0, 0, 0.1));
+      backdrop-filter: blur(59.2764px);
+      border-radius: ${conv(32, "vh")};
+      overflow: hidden;
+    }
 
     article.lata-image {
       height: 100%;
@@ -118,8 +172,8 @@ const Style = {
         flex-direction: column;
         align-items: flex-start;
 
-        h2,
-        h3 {
+        h4,
+        h5 {
           width: 100%;
         }
       }
