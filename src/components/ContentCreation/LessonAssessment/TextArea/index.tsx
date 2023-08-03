35dd1c18c5907602d33af4a027b5ccb45760d032
nmomoh <nobert@groovejones.com>
2023-08-03 07:37:08 +0100

    style fixes to lesson assessment

--- src/components/ContentCreation/LessonAssessment/TextArea/index.tsx
+++ src/components/ContentCreation/LessonAssessment/TextArea/index.tsx
@@ -54,7 +54,7 @@ export const LessonAssessmentTextAreaComponent = ({
             name="overview"
           />
         </h2>
-        <Typography tag="h3" size="3.5vh" color="neutral-700">
+        <Typography tag="h3" size="2.5vh" color="neutral-700">
           <DoubleClickToEditComponent
             mode={state.lessonName.mode}
             setText={changeText}
@@ -128,18 +128,21 @@ const Style = {
 
     .la-top {
       width: 100%;
-      height: 15%;
+      height: 17.5%;
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
 
       h2 {
         font-weight: 600;
-        font-size: ${conv(14, "vh")};
+        font-size: ${conv(12, "vh")};
         text-transform: uppercase;
         color: var(--orange-600);
         border: 2px solid var(--orange-600);
-        padding: ${conv(10, "vh")} ${conv(30, "vw")};
+        padding: 5px 15px;
         border-radius: 24px;
         width: max-content;
-        margin-bottom: 10px;
       }
     }
 
